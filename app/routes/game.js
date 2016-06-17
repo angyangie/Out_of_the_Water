82fb3946ec070730937c1901ef6c8637fa6764ce
Michael Appell <mikeappell@gmail.com>
2016-06-17 13:24:50 -0400

    Created second board added bootstrap added second table to reflect second player's board added logic that properly parses model object for a player's ships

--- app/routes/game.js
+++ app/routes/game.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findRecord('game', 1);
-
-    //What does this look like?? -- need to figure out how to grab params!!!
-
-    // return [["Cruiser","33","34","35"],["Destroyer","66","56"],["Battleship","92","93","94","95"],["Carrier","28","38","48","58","68"],["Battleship", "11","12","13","14"]];
   },
   actions: {
     makeHit(params) {
@@ -15,7 +11,7 @@ export default Ember.Route.extend({
       let hit = false;
       for (let i = 0;i < model.length;i++) {
         for (let j = 0;j < model[i].length;j++) {
-          if (model[i][j] === params) { //need to figure out how to grab params from the API!! (see above)
+          if (model[i][j] === params) {
             hit = true;
             hits[params[0]][params[1]] = true;
           }
