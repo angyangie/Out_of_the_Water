dd4e2aa0923863a85e0491b930b062a391db7065
angyangie <angie.ramirez12@gmail.com>
2016-06-20 17:46:14 -0400

    AI working AIfunction placed in game controller AIfunction called in makeHit action in route.js. AI plays alongside human player Still debugging AI board issues

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -14,6 +14,56 @@ export default Ember.Controller.extend({
       innerArray = [];
     }
     return cArray;
+  }),
+  aiFunction: Ember.computed('model.userGames.@each.hits', function() {
+    debugger;
+    const hits = this.store.peekAll('user_game').mapBy('hits')[0].split(",");
+
+    var allHitsCoordinates = []
+    hits.forEach(function(hit, index){
+      if (hit == "2") {
+        allHitsCoordinates.push(index)
+      }
+    })
+
+    var allHitsCoordinatesSort = allHitsCoordinates.sort();
+
+    var allNonMisses = []
+    hits.forEach(function(hit, index){
+      if (hit == "0") {
+        allNonMisses.push(index)
+      }
+    })
+
+    if (allHitsCoordinates.length == 0){
+
+      return (allNonMisses[Math.floor(Math.random() * allNonMisses.length)]).toString()
+
+    } else if (allHitsCoordinates.length == 1) {
+
+      let surroundingArray = [[allHitsCoordinates[0] + 10],[allHitsCoordinates[0] - 10], [allHitsCoordinates[0] + 1], [allHitsCoordinates[0] - 1]]
+
+      for (let i = 0;i < surroundingArray.length;i++) {
+        if (hits[surroundingArray[i][0]] === "0") {
+          let attack_coords = surroundingArray[i][0];
+          return (attack_coords < 10 ? `0${attack_coords}` : attack_coords.toString());
+        }
+      }
+
+    } else if (allHitsCoordinates.length > 1){
+
+      var vertical = allHitsCoordinatesSort[0].toString()[1] === allHitsCoordinatesSort[1].toString()[1]
+
+      var lastCoord = allHitsCoordinatesSort[allHitsCoordinatesSort.length - 1]
+      var firstCoord = allHitsCoordinatesSort[0]
+
+      let nextValue1 = (vertical ? parseInt(lastCoord) + 10 : parseInt(lastCoord) + 1)
+
+      let nextValue2 = (vertical ? parseInt(firstCoord) - 10 : parseInt(firstCoord) - 1)
+
+      return (hits[nextValue1] == 0 ? nextValue1.toString() : nextValue2.toString())
+
+    }
   })
 });
 
--- app/routes/game.js
+++ app/routes/game.js
@@ -4,32 +4,9 @@ export default Ember.Route.extend({
   model() {
     return this.store.findRecord('game', 1);
   },
-  isHit(coords) {
-    const user_game = this.store.peekRecord('user_game', 1)
-
-    const hits_array = user_game.get('hits');
-    coords = [parseInt(coords[0][0]), parseInt(coords[0][1])];
-    
-    if (hits[coords[0]][coords[1]]){
-      return "<img src='assets/images/miss.jpg'>"
-    }
-  },
-
   actions: {
     makeHit(params) {
-
-      // AI call goes here
-
-      let ai_coords = aiFunction(this.model)
-
-      function aiFunction(model) {
-
-
-        //Do AI logic based on which grid squares have been attacked, and also whether its most recent shot was a hit or a miss
-        //Do logic, random grid square or adjacent square to most recent shot, if most recent shot was a hit
-        //Return coordinates to attack next
-      }
-
+      let ai_coords = this.get('controller').get('aiFunction');
       let data = {
         url: 'http://localhost:3000/api/v1/games/hits_array',
         type: 'post',
@@ -38,7 +15,7 @@ export default Ember.Route.extend({
         data: {
           user_game_id: 1,
           player_coords: params,
-          ai_coords: "11"
+          ai_coords: ai_coords
         }
       }
 
