1fc9ae28970e54d20ff5c5205ef1ea766b28a189
Michael Appell <mikeappell@gmail.com>
2016-06-30 23:43:18 -0400

    Fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,8 +8,8 @@ module.exports = function(defaults) {
       // enabled: false
       generateAssetMap: true
     }
-  })
-};
+  });
+}
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
