49400343c9cefccf154c3c591b8427142bad517b
Michael Appell <mikeappell@gmail.com>
2016-06-30 23:46:11 -0400

    Fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      // enabled: false
       generateAssetMap: true
     }
   });
-}
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
